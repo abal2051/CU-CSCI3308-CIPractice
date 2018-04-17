d7ff5fc42768bb683e1f924858e1228aeac677fa
AKA <abal2051@colorado.edu>
2018-04-17 14:40:48 -0600

    Added travis

--- geometry.c
+++ geometry.c
@@ -69,3 +69,23 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+
+
+/*
+
+*/
+
+double coord_2d_area_triangle(const coord_2d_t* a, const 
+coord_2d_t* b, const coord_2d_t* c){
+
+    if((a->x < 0)||(a->y<0)||(b->x<0)||(b->y<0)||(c->x<0)||(b->y<0)){
+        return -1;
+    }
+
+double val = ((a->x)*(b->y - c->y) + (b->x)*(c->y- a->y)+(c->x)*(a->y -b->y))/2;
+val = fabs(val);
+printf("%lf \n", val);
+return val;
+
+
+}
